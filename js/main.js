6be4b198065d1ba021e8c401a2c19e65f8857957
Elliot Hwang <elliothwang@MacBook-Pro-2.local>
2021-02-28 16:28:16 -0800

    test commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// ===== Minimum Requirements ===== //
+// Display an empty tic-tac-toe board when the page is initially displayed.
+// A player can click on the nine cells to make a move.
+// Every click will alternate between marking an X and O.
+// Once occupied with an X or O, the cell cannot be played again.
+// Provide a Reset Game button that will clear the contents of the board.
+
+
+
+
+// 1) Define required constants
+
+// 2) Define required variables used to track the state of the game
+
+// 3) Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable and performant.
+
+// 4) Upon loading the app should:
+// 	4.1) Initialize the state variables
+// 	4.2) Render those values to the page
+// 	4.3) Wait for the user to click a square
+
+// 5) Handle a player clicking a square
+
+// 6) Handle a player clicking the replay button
---
+++ readme.md
@@ -0,0 +1,7 @@
+<!-- Saving Your Work to the Repo
+
+1. git add -A
+
+2. git commit -m ""
+
+3. git push -u origin master (the first push), then just git push from then on thanks to the -u option.
